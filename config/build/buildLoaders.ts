2557bd51a1a1863dcafdae1f5a5c41ee23bd6dcf
Kamran Aghayev <kamran.aghayev@todor3d.tech>
2025-05-09 16:19:11 +0400

    add wp assets loaders(png, svg, jpg, jpeg)

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -5,14 +5,42 @@ import { BuildOptions } from "./types/types";
 export function buildLoaders(options: BuildOptions): ModuleOptions["rules"] {
   const isDev = options.mode === "development";
 
-const cssLoaderWithModules = {
+  const svgrLoader = {
+    test: /\.svg$/i,
+    issuer: /\.[jt]sx?$/,
+    use: [
+      {
+        loader: "@svgr/webpack",
+        options: {
+          icon: true,
+          svgoConfig: {
+            plugins: [
+              {
+                name: "convertColors",
+                params: {
+                  currentColor: true,
+                },
+              },
+            ],
+          },
+        },
+      },
+    ],
+  };
+
+  const assetLoader = {
+    test: /\.(png|jpg|jpeg|gif)$/i,
+    type: "asset/resource",
+  };
+
+  const cssLoaderWithModules = {
     loader: "css-loader",
     options: {
       modules: {
-        localIdentName: isDev ? "[path][name]__[local]" : "[hash:base64:8]"
-      }
+        localIdentName: isDev ? "[path][name]__[local]" : "[hash:base64:8]",
+      },
     },
-}
+  };
 
   const scssLoader = {
     test: /\.s[ac]ss$/i,
@@ -32,5 +60,5 @@ const cssLoaderWithModules = {
     exclude: /node_modules/,
   };
 
-  return [scssLoader, tsLoader];
+  return [svgrLoader, assetLoader, scssLoader, tsLoader];
 }
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import classes from "./app.module.scss";
 import { Outlet, Link } from "react-router-dom";
 import About from "@/pages/about/About";
+import img_png from "@/assets/img_png.png";
+import Img_svg from "@/assets/img_svg.svg";
+import img_jpg from "@/assets/img_jpg.jpg";
 
 export const App = () => {
   const [count, setCount] = useState<number>(0);
@@ -9,6 +12,11 @@ export const App = () => {
   return (
     <div>
       <div>
+        <div>
+          <img width={100} height={100} src={img_png} alt="img_png" />
+          <img width={100} height={100} src={img_jpg} alt="img_jpg" />
+          <Img_svg width={100} height={100} />
+        </div>
         <Link to={"/about"}>About</Link>
         <Link to={"/shop"}>Shop</Link>
         <p className={classes.value}>{count}</p>
